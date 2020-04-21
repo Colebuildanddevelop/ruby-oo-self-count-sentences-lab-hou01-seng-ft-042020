82377359ddd2a69b547114e532644c5c88700045
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-04-21 18:33:37 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    prev_char = ""
+    counter = 0
+    self.split('').map do |char|
+      if (char == "!" || char == "?" || char == ".") && prev_char != "!" && prev_char != "?" && prev_char != "."
+        counter = counter + 1
+      end
+      prev_char = char
+    end 
+    counter
   end
 end
